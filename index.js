445719ad99dd3ec1d220bdc23b4b5019c2fa9ed7
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-02-03 12:12:55 +0500

    Start

--- index.js
+++ index.js
@@ -227,6 +227,6 @@ app.get("/ping", async (req, res) => {
 app.use(bot.webhookCallback(webhookPath));
 
 // === HTTP serverni ishga tushirish ===
-server.listen(8000, () => {
-  console.log("Server 3001 portda ishga tushdi");
+server.listen(8001, () => {
+  console.log("Server 8001 portda ishga tushdi");
 });
