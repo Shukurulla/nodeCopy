7a5f30c4918bdb5146a45b61ab033af7c1a81964
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-05-20 15:02:13 +0500

    change click qrcode ling

--- router/click.routes.js
+++ router/click.routes.js
@@ -266,7 +266,7 @@ router.post("/get-click-link", async (req, res) => {
         message: "iltimos malumotlarni toliq kiriting",
       });
     }
-    const qrCode = `http://api.qrserver.com/v1/create-qr-code/?data=https://my.click.uz/services/pay?service_id=${process.env.CLICK_SERVICE_ID}&merchant_id=${process.env.CLICK_MERCHANT_ID}&amount=${amount}&transaction_param=${orderId}&size=x&bgcolor=`;
+    const qrCode = `https://my.click.uz/services/pay?service_id=71257&merchant_id=38721&amount=${amount}&transaction_param=${orderId}`;
     return res.json({ status: "success", data: qrCode });
   } catch (error) {
     res.status(500).json({ status: "error", message: error.message });
